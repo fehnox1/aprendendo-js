0748d98fde1891fb4ee92c81b74418c98a774b15
Fernando Brigida <105243988+fehnox1@users.noreply.github.com>
2024-01-11 19:07:22 -0300

    aula6

--- Js/aula6.js
+++ Js/aula6.js
@@ -1,2 +1,22 @@
-//variaveis
+//variaveis -> var, let , const 
 
+//declaração de variaveis de forma certa.
+//var a,b,c;
+//a = 5;
+//b = 1;
+//c = a + b;
+//alert(c);
+//declaração de valores de forma "errada"
+//var a = 5;
+//var b = 1;
+//var c = a + b;
+//alert(c);
+
+//var nome, sobrenome, nomeCompleto;
+//nome = Fernando;
+//sobrenome = Brigida;
+//nomeCompleto  = nome + sobrenome;
+//document.getElementById("texto").innerHTML = nomeCompleto;
+//=> Diferenças let torna variavel mode ser mutavel dentro de blocos. mas nunca fora.
+//-> const é a regra mais restrita de todas, é imutavel.    <- quando você precisa de ser valor único. e nunca vai ser mutavel
+    
