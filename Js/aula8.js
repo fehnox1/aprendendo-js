643ab0995ac148dd22c5b42b2c89231b5f77d81d
Fernando Brigida <105243988+fehnox1@users.noreply.github.com>
2024-01-12 18:12:07 -0300

    aula8.3

--- Js/aula8.js
+++ Js/aula8.js
@@ -8,4 +8,10 @@ function soma(valor1, valor2, valor3){
 
 document.getElementById("texto").innerHTML  = soma(10, 500);
 - segundo jeito */
-    
+function soma (valor1, valor2, valor3){
+    return valor1 + valor2
+}
+
+var total = soma(10, 23);
+
+alert(total)
