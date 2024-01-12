472fa4663d4be8c15e55d5414d948eaf05c227db
Fernando Brigida <105243988+fehnox1@users.noreply.github.com>
2024-01-12 18:09:56 -0300

    aula8.3

--- Js/aula8.js
+++ Js/aula8.js
@@ -1,5 +1,11 @@
 /*Funções  é um procedimento de JavaScript - um conjunto de instruções que executa uma tarefa ou calcula um valor.
-*/
+
+
+- primeiro jeito de fazer ! function
 function soma(valor1, valor2, valor3){
-    
+    return valor1 + valor2; 
 }
+
+document.getElementById("texto").innerHTML  = soma(10, 500);
+- segundo jeito */
+    
