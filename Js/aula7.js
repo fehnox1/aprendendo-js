cb60a59ce7c03b968982dd727fd3841f8d414f56
Fernando Brigida <105243988+fehnox1@users.noreply.github.com>
2024-01-11 21:46:28 -0300

    aula7

---
+++ Js/aula7.js
@@ -0,0 +1,38 @@
+/* Operadores Matematicos
+Sinais que usamos => + - / = ++ -- += -= && || etc..
+1) Operadores Aritmeticos Matematicos
+2) Operadores de Atribuição 
+3) Operadores de Sequencia
+4) Operadores de Comparação
+5) Operador Condicional (Ternario)
+6) Operadores Logicos
+
+*/
+
+/*var numero1, numero2, resultado
+
+numero1 = 5;
+numero2 = 6
+resultado = numero1 + numero2 
+alert(resultado)
+também podemos adicionar ++ para acrementar.. exemplo:
+*/
+/*var nu1, nu2, resultado;
+
+nu1 = 5 ;
+nu2 = 4;
+
+resultado = ++nu1 + nu2; 
+alert(resultado);
+var nu1, nu2, resultado;
+n1 = 5;
+n2 = 4;
+vai receber a variavel dentro de outra.
+nu1 += nu2; */
+// comparação ou true ou false
+var valor1, valor2, resultado;
+valor1 = 8;
+valor2 = 10;
+resultado = (valor1 == valor2);
+alert(resultado);
+
