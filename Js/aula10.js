84d347a1bb1c618043679cdc1fa0345b4b3bc86f
Fernando Brigida <105243988+fehnox1@users.noreply.github.com>
2024-01-13 17:47:17 -0300

    aula10.1

---
+++ Js/aula10.js
@@ -0,0 +1,18 @@
+/* EVENTOS
+Eventos são ações disparadas pela interação dos usuários na página.
+é o correto manejo desses eventos que tornam as páginas interativas e 
+dinâmicas.
+
+Existem muitos eventos. Veja os mais utilizados:
+
+onclick     ->   Disparado quando recebe um Clicque
+ondbclick   ->   Disparado quando clique duplo.
+onmouseover ->   Disparado quando o mouse está sobre.
+onmouseout  ->   Disparado quando o mouse é movio para fora do elemento.
+onmousemove ->   Disparado quando o mouse é movido no elemento.
+onmousedown ->   Disparado quando o clique do botão foi pressionado.
+onmouseup   ->   Diapsparado quando o clique do botão é liberado.
+onfocus     ->   Disparado quando o elemento recebe o foco. válido para input, 
+onchange    ->   Disparado quando existe uma mudança no contéudo. ''Ao Mudar''.
+onblur      ->   Disparado quando o elemento perde o foco.
+*/
