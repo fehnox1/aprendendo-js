02b2e1c245b3c8d88f9682a560c18f40054755f5
Fernando Brigida <105243988+fehnox1@users.noreply.github.com>
2024-01-14 16:02:08 -0300

    aula10.02

--- Js/aula10.js
+++ Js/aula10.js
@@ -15,4 +15,15 @@ onmouseup   ->   Diapsparado quando o clique do botão é liberado.
 onfocus     ->   Disparado quando o elemento recebe o foco. válido para input, 
 onchange    ->   Disparado quando existe uma mudança no contéudo. ''Ao Mudar''.
 onblur      ->   Disparado quando o elemento perde o foco.
+
+
+//primeiro evento > onclick
+function evento(){
+    alert("Acionou um evento de clique! onclick")
+    document.body.style.backgroundColor = "yellow";
+}
 */
+    function evento1    ()  {
+    alert   (   "Acionou um evento de clique! ondbclick"    )
+    document.body.style.backgroundColor     =    "green";
+    }
