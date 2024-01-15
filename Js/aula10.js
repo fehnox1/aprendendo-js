52ee0b6ada64bb7bf49faf83ef0ed83f1651a6a6
Fernando Brigida <105243988+fehnox1@users.noreply.github.com>
2024-01-15 16:49:22 -0300

    aula10.s2

--- Js/aula10.js
+++ Js/aula10.js
@@ -25,5 +25,5 @@ function evento(){
 */
     function evento1    ()  {
     alert   (   "Acionou um evento de clique! ondbclick"    )
-    document.body.style.backgroundColor     =    "green "; s2
+    document.body.style.backgroundColor     =    "green ";
     }
