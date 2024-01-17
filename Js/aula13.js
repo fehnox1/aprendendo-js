ea37be32d6e4ad71053442b4cedb7a5d65d23631
Fernando Brigida <105243988+fehnox1@users.noreply.github.com>
2024-01-17 19:36:19 -0300

    aula13.horasNoSite

--- Js/aula13.js
+++ Js/aula13.js
@@ -1,9 +1,25 @@
 // iff e else   
 
-var interruptor = "off";
+/*var interruptor = "off";
 
 if(interruptor == "on"){
         alert ("a lampada  está ligada");
 } else {
     alert("a lampada está desligada!");
+} */
+
+
+
+// hora pro site!
+var hora = new Date().getHours();
+
+if (hora < 12){
+    alert("Bom dia!");
+
+} else if (hora < 18) {
+    alert("Boa tarde!");
+    
+} else {
+
+    alert("Boa noite!");
 }
